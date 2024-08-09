5a93187a96385deb8bc016290d98835916d866a7
kewldan <kewldanil1@gmail.com>
2024-08-09 15:58:35 +0300

    config support

---
+++ src/config.py
@@ -0,0 +1,30 @@
+import json
+import os.path
+from typing import Literal
+
+from pydantic import BaseModel
+
+
+class Config(BaseModel):
+    width: int = 128
+    height: int = 40
+    transition: Literal['disabled', 'circle', 'slide'] = 'circle'
+    refresh_rate: int = 10
+    music_refresh_rate: int = 20
+    events_duration: float = 1.
+    text_speed: float = 6.
+    row_gap: int = 6
+
+    def __init__(self) -> None:
+        if os.path.exists('config.json'):
+            with open('config.json', 'r', encoding='utf-8') as f:
+                data = json.load(f)
+                super().__init__(**data)
+        else:
+            super().__init__()
+
+        with open('config.json', 'w', encoding='utf-8') as f:
+            json.dump(self.model_dump(), f, sort_keys=True, indent=4)
+
+
+config = Config()
--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ import time
 from contextlib import suppress
 
 import media
+from config import config
 from gamesense import GameSense
 from gtk import BasicGTK
 from gtk.manager import DrawMode
@@ -19,23 +20,23 @@ async def draw_thread(screen: OLEDScreen):
     event_start = None
 
     while True:
-        sleep = 0.1
+        sleep = 1 / config.refresh_rate
 
         async with BasicGTK(screen) as gtk:
-            if media.latest_event in ['resumed', 'paused']:
+            if config.events_duration > 0 and media.latest_event in ['resumed', 'paused']:
                 gtk.draw_center_text(2, media.latest_event)
                 media.latest_event = None
-                sleep = 1
+                sleep = config.events_duration
             else:
                 if media.latest_media:
-                    gtk.draw_big_text(0, media.latest_media.title, 6.)
-                    gtk.draw_big_text(12, media.latest_media.artist, 6.)
+                    gtk.draw_big_text(0, media.latest_media.title, config.text_speed)
+                    gtk.draw_big_text(7 + config.row_gap, media.latest_media.artist, config.text_speed)
 
                     position_progress = (
                             media.latest_media.calculated_position.total_seconds() / media.latest_media.end.total_seconds())
 
-                    gtk.draw_progress(2, 24, screen.size[0] - 4, 10, position_progress)
-                    gtk.draw_center_text(26,
+                    gtk.draw_progress(2, 14 + config.row_gap * 2, screen.size[0] - 4, 10, position_progress)
+                    gtk.draw_center_text(14 + config.row_gap * 2 + 2,
                                          f'{format_seconds(media.latest_media.calculated_position.total_seconds())} - {format_seconds(media.latest_media.end.total_seconds())}',
                                          DrawMode.FLIP)
                 else:
@@ -46,8 +47,12 @@ async def draw_thread(screen: OLEDScreen):
                     event_start = time.time()
 
                 elapsed = time.time() - event_start
-                gtk.draw_circle(64, 20, elapsed * 50, DrawMode.FLIP)
-                gtk.draw_circle(64, 20, (elapsed - 0.4) * 50, DrawMode.FLIP)
+
+                if config.transition == 'circle':
+                    gtk.draw_circle(screen.size[0] // 2, screen.size[1] // 2, elapsed * 50, DrawMode.FLIP)
+                    gtk.draw_circle(screen.size[0] // 2, screen.size[1] // 2, (elapsed - 0.4) * 50, DrawMode.FLIP)
+                elif config.transition == 'slide':
+                    pass
 
                 if time.time() - event_start > 2:
                     media.latest_event = None
@@ -59,11 +64,11 @@ async def draw_thread(screen: OLEDScreen):
 async def fetch_music_thread() -> None:
     while True:
         await media.get_media_info()
-        await asyncio.sleep(0.05)
+        await asyncio.sleep(1 / config.music_refresh_rate)
 
 
 async def main():
-    size = (128, 40)
+    size = (config.width, config.height)
 
     gs = GameSense('GRAPHICS', 'Music display', 'kewldan')
     await gs.register_game()
